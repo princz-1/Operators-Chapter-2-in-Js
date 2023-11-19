9e523ad14e158e474ab8d36a0814b197812f8c55
Princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-19 16:36:47 +0530

    Logical-Operator.js
    /* Logical Operator
    
    - Logical AND (&&)
    - Logical OR (||)
    - Logical NOT (!)
    
    */

---
+++ Logical-Operator.js
@@ -0,0 +1,26 @@
+/* Logical Operator
+
+- Logical AND (&&)
+- Logical OR (||)
+- Logical NOT (!)
+
+*/
+
+// Logical AND (&&)
+let a =6;
+let b =5;
+let cond1 = a > b; // True
+let cond2 = a===5; // False
+console.log("cond1 && cond2 =",cond1 && cond2); // False
+
+// Logical OR (||)
+let c =6;
+let d =5;
+let condi1 = c > d; // True
+let condi2 = c===5; // False
+console.log("condi1 || condi2 =",condi1 || condi2); // True
+
+// Logical NOT (!)
+let e =6;
+let f =5;
+console.log("!(e < f) =",!(e < f)); // True
