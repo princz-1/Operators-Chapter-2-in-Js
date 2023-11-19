57d9b07adaf62694f14c5d2353230f20c90836de
Princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-19 17:42:41 +0530

    Ternary-Operators.js
    /* Ternary Operators
    - Condition ? true output : false output
    
    */

---
+++ Ternary-Operators.js
@@ -0,0 +1,8 @@
+/* Ternary Operators
+- Condition ? true output : false output
+
+*/
+
+let age = 18;
+let result = age >= 18 ? "Adult" : "Not adult"; // simpler,compact if-else
+console.log(result);
